c02dc0b690d9cfe8e1130f53be70855f394a5c9a
Spencer Rogers <spencer1248@gmail.com>
2015-06-24 00:14:15 -0400

    Add schedule page

--- app.rb
+++ app.rb
@@ -13,4 +13,28 @@ class App < Sinatra::Base
 
     erb :reddit
   end
+
+  get '/schedule' do
+    @today = [
+      ['7:00am', 'Wake up'],
+      ['8:00am', 'Work Out'],
+      ['9:00am', 'Product Meeting'],
+      ['11:00am', 'Ping Pong Break'],
+      ['1:00pm', 'Lunch'],
+      ['3:00pm', 'Coffee Time'],
+      ['6:30pm', 'Call it a day'],
+    ]
+
+    @tomorrow = [
+      ['7:00am', 'Wake up'],
+      ['8:00am', 'Work Out'],
+      ['9:00am', 'Inbox Zero'],
+      ['11:00am', 'Ping Pong Break'],
+      ['1:00pm', 'Lunch'],
+      ['3:00pm', 'Coffee Time'],
+      ['6:30pm', 'Meetup Presentation'],
+    ]
+
+    erb :schedule
+  end
 end
